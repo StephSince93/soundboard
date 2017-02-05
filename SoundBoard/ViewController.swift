38ac0e1dc159b4be2171d0e8a474ba8ffe147790
Stephen Romero <stephen.romero93@gmail.com>
2017-02-04 23:59:36 -0600

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Stephen Romero on 2/4/17.
+//  Copyright © 2017 Stephen Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
