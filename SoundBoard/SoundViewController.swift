38f81f76a18dbbbd8fe682c494eaf930972119f3
Stephen Romero <stephen.romero93@gmail.com>
2017-02-05 00:21:32 -0600

    basic setup

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by Stephen Romero on 2/5/17.
+//  Copyright © 2017 Stephen Romero. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any)
+    {
+        
+        
+    }
+  
+    @IBAction func playTapped(_ sender: Any)
+    {
+        
+        
+    }
+    
+    @IBAction func addTapped(_ sender: Any)
+    {
+        
+        
+    }
+    
+}
